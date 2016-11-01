284d66a2718302208cab98d68a758520e8e52be2
Oliver Radcliffe <ojradcliffe@gmail.com>
2016-11-01 10:06:04 -0500

    added new files

---
+++ Deploy_template.ps1
@@ -0,0 +1,12 @@
+﻿$resourcegroupname = RG1
+$resourcegroupdeploymentname = RG1deployment1
+
+
+#Login to azure
+Login-AzureRmAccount
+
+# create a new resource group
+New-AzureRmResourceGroup -Name $resourcegroupname -Location "Central US"
+
+# deploy the template to the resource group
+New-AzureRmResourceGroupDeployment -Name $resourcegroupdeploymentname -ResourceGroupName $resourcegroupname -TemplateFile simple_deployment.json -TemplateParameterFile simple_deployment.params.json
